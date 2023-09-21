1004ce368c651fcd7d0edde17d6d668aca19454b
HowKwan <87495440+HowKwan@users.noreply.github.com>
2023-09-21 10:47:42 +0700

    Cai dat xu li loi

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-
 const express = require("express");
-const  cors = require ("cors");
+const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+    });
+});
 
 app.get("/", (req, res) => {
-    res.json({message: "welcome to contact book application."});
+    res.json({ message: "Hello world!"});
 });
 
 module.exports = app;
